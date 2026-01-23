0bb9d8e2e1158d0c4fb6745e40974ea543c5cd4e
lathoiful <lathoiful0508@gmail.com>
2026-01-24 00:49:59 +0700

    Enhance volume control functionality with mute and pause features

--- main.py
+++ main.py
@@ -19,6 +19,10 @@ devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
 
+last_volume = 50
+is_muted = False
+is_paused = False
+
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -32,28 +36,45 @@ while True:
                 lmList.append((int(lm.x * w), int(lm.y * h)))
 
             if lmList:
-                x1, y1 = lmList[4]   
-                x2, y2 = lmList[8]   
-                cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
-
-                cv2.circle(img, (x1, y1), 10, (255, 0, 255), cv2.FILLED)
-                cv2.circle(img, (x2, y2), 10, (255, 0, 255), cv2.FILLED)
-                cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 2)
-                cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
-
+                x1, y1 = lmList[4]
+                x2, y2 = lmList[8]
                 length = np.hypot(x2 - x1, y2 - y1)
 
                 volPercent = np.interp(length, [20, 200], [0, 100])
-                volume.SetMasterVolumeLevelScalar(volPercent / 100, None)
+                last_volume = volPercent
+                if not is_muted:
+                    volume.SetMasterVolumeLevelScalar(volPercent/100, None)
 
-                volBar = np.interp(volPercent, [0, 100], [400, 150])
-                cv2.rectangle(img, (50, 150), (85, 400), (0, 255, 0), 3)
-                cv2.rectangle(img, (50, int(volBar)), (85, 400), (0, 255, 0), cv2.FILLED)
+                if length < 30:
+                    is_paused = not is_paused
+                    cv2.putText(img, "PAUSE/PLAY toggled", (200,50),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+                    cv2.waitKey(300)
 
-                cv2.putText(img, f'Vol: {int(volPercent)} %', (40, 450),
-                            cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 3)
+                elif length > 150:
+                    is_muted = not is_muted
+                    if is_muted:
+                        volume.SetMasterVolumeLevelScalar(0, None)
+                    else:
+                        volume.SetMasterVolumeLevelScalar(last_volume/100, None)
+                    cv2.putText(img, f"MUTE toggled: {is_muted}", (200,80),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+                    cv2.waitKey(300)
 
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    else:
+        if not is_muted:
+            volume.SetMasterVolumeLevelScalar(last_volume/100, None)
+        cv2.putText(img, "No hand detected, using last volume", (50,50),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,0), 2)
+
+    volBar = np.interp(last_volume, [0, 100], [400, 150])
+    color = (int(last_volume*2.55), 255 - int(last_volume*2.55), 0)
+    cv2.rectangle(img, (50, 150), (85, 400), (0, 255, 0), 3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), color, cv2.FILLED)
+    cv2.putText(img, f'Vol: {int(last_volume)} %', (40, 450),
+                cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 3)
 
     cv2.imshow("Volume Control", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
